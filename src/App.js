f393d637c8c8d7197e6ddab30d14f5b83770f474
sedav48 <62444775+sedav48@users.noreply.github.com>
2022-04-06 09:28:00 -0400

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Directory from './components/DirectoryComponent';    
+    
+   
 
 
 class App extends Component {
@@ -12,6 +15,7 @@ class App extends Component {
           <NavbarBrand href="/">NuCamp</NavbarBrand>
         </div>
         </Navbar>
+        <Directory />
       </div>
     );
   }
