2e8391e820804103236d20a65c5bf2032274a0a0
sedav48 <62444775+sedav48@users.noreply.github.com>
2022-04-13 14:23:20 -0400

    React Router

--- src/components/MainComponents.js
+++ src/components/MainComponents.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
 
@@ -12,24 +14,39 @@ class Main extends Component {
         super(props);
         this.state = {
             campsites: CAMPSITES,
-            selectedCampsite: null
+            
         };
     }
+   
+    render () {
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        };
+    
 
-    onCampsiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId});
-    }
-
-    render() {
         return (
             <div>
                 <Header />
-                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Switch>
+                    <Route path ='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Redirect to='/home' />
+                </Switch>
                 <Footer />
             </div>
         );
-    };
-}
+    }
+        return () {
+        <div>
+            <Header />
+            <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
+            <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id ===this.state.selectedCampsite)[0]} />
+            <Footer />
+        </div> 
+    }
 
+};
+    
 export default Main;
