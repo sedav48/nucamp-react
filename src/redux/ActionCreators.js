d412aa931b1020b6fdf495df76c8b08b7bc40d61
sedav48 <62444775+sedav48@users.noreply.github.com>
2022-04-30 18:06:56 -0400

    Workshop 5

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -3,6 +3,7 @@ import { baseUrl } from '../shared/baseUrl';
 
 
 
+
 export const fetchCampsites = () => dispatch => {
     
    dispatch(campsitesLoading());
@@ -81,7 +82,7 @@ export const addComment = comment => ({
     payload: comment
 });
 
-export const postComment = (campsiteId, rating, author, text) => dispatch => {
+ export const postComment = (campsiteId, rating, author, text) => dispatch => {
     const newComment = {
         campsiteId: campsiteId,
         rating: rating,
@@ -107,6 +108,7 @@ export const postComment = (campsiteId, rating, author, text) => dispatch => {
             }
         },
         error => { throw error; }
+        
         )
         .then(response => response.json())
         
@@ -156,3 +158,85 @@ export const addPromotions = promotions => ({
     type: ActionTypes.ADD_PROMOTIONS,
     payload: promotions
 });
+
+export const fetchPartners = () => dispatch => {
+    
+    dispatch(partnersLoading());
+ 
+    return fetch(baseUrl + 'partners')
+     .then(response => {
+         if (response.ok) {
+             return response;
+         } else {
+             const error = new Error(`Error ${response.status}: ${response.statusText}`);
+             error.response = response;
+             throw error; 
+         }
+     }, 
+     error => {
+         const errMess = new Error(error.message);
+         throw errMess;
+         }
+     )
+     .then(response => response.json())
+     .then(partners => dispatch(addPartners(partners)))
+     .catch(error => dispatch(partnersFailed(error.message)));
+     
+ };
+
+
+
+export const partnersLoading = partners => ({
+    type: ActionTypes.PARTNERS_LOADING,
+    
+});
+
+export const partnersFailed = errMess => ({
+    type: ActionTypes.PARTNERS_FAILED,
+    payload: errMess
+   
+});
+
+export const addPartners= partners => ({
+    type: ActionTypes.ADD_PARTNERS,
+    payload: partners
+    
+});
+
+export const postFeedback = (feedback) => {
+
+    return fetch(baseUrl + 'feedback', {
+        method: "POST",
+        body: JSON.stringify(feedback),
+        headers: {
+            "Content-type" : "application/json"
+        }
+    })
+
+    .then(response => {
+       if (response.ok) {
+        } else {
+        
+        const error = new Error(`Error ${response.status}: ${response.statusText}`);
+        error.response = response;
+        throw error;
+
+        }
+    }, 
+     error => { throw error; }
+)
+
+    .then(response => response.json())
+    .catch(response => {
+        console.log(`post comment`, response);
+        alert('Thank you for your feedback ' + response);
+    })
+
+    .catch(error => {
+        console.log(`post comment`, error.message);
+        alert(`Your feedback is not shown\nError: ' + error.message`);
+    });
+
+};
+
+
--- src/redux/partners.js
+++ src/redux/partners.js
@@ -1,8 +1,21 @@
 import { PARTNERS } from '../shared/partners';
+import * as ActionTypes from './ActionTypes';
 
 export const Partners = (state = PARTNERS, action) => {
     switch (action.type) {
-        default:
-            return state;
-    }
-};
+        
+            case ActionTypes.ADD_PARTNERS:
+                return { ...state, errMess: null, partners: action.payload};
+            
+            case ActionTypes.PARTNERS_FAILED:
+                return { ...state, errMess: action.payload};
+    
+            case ActionTypes.PARTNERS_LOADING:
+                const partner = action.payload;
+               
+                return {...state, partners: state.partners.concat(partner)};
+            default:
+                return state;
+        }
+    };
+ 
